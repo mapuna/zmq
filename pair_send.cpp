0ecb5a75017436bf7bf042fcda47d520ea244eed
Anupam Gupta <mapuna_@outlook.com>
2023-09-05 23:02:53 +0530

    added subscribe server

--- pair_send.cpp
+++ pair_send.cpp
@@ -5,32 +5,32 @@
 #include <zmq.hpp>
 
 int main(int argc, char *argv[]) {
-  zmq::context_t context(1);
-  zmq::socket_t ps(context, ZMQ_PAIR);
-  ps.bind("tcp://*:5555");
+    zmq::context_t context(1);
+    zmq::socket_t ps(context, ZMQ_PAIR);
+    ps.bind("tcp://*:5555");
 
-  if (argc < 2) {
-    std::cerr << "Please specify image path." << std::endl;
-    throw std::runtime_error("Please specify the file to open.\n");
-  }
+    if (argc < 2) {
+        std::cerr << "Please specify image path." << std::endl;
+        throw std::runtime_error("Please specify the file to open.\n");
+    }
 
-  cv::Mat img = cv::imread(argv[1], cv::IMREAD_COLOR);
-  // The shitbag called OpenCV never throws an error, so need to check.
-  if (img.empty()) {
-    std::cout << "File path does not exist." << std::endl;
-    exit(EXIT_FAILURE);
-  }
+    cv::Mat img = cv::imread(argv[1], cv::IMREAD_COLOR);
+    // The shitbag called OpenCV never throws an error, so need to check.
+    if (img.empty()) {
+        std::cout << "File path does not exist." << std::endl;
+        exit(EXIT_FAILURE);
+    }
 
-  std::vector<uchar> buffer;
+    std::vector<uchar> buffer;
 
-  // encode the read image as JPEG bytes into the byte buffer
-  cv::imencode(".jpg", img, buffer);
-  // convert the buffer to string.
-  std::string img_str(buffer.begin(), buffer.end());
-  zmq::message_t message(img_str.size());
-  // copy the data to the message
-  memcpy(message.data(), img_str.data(), img_str.size());
-  ps.send(message);
+    // encode the read image as JPEG bytes into the byte buffer
+    cv::imencode(".jpg", img, buffer);
+    // convert the buffer to string.
+    std::string img_str(buffer.begin(), buffer.end());
+    zmq::message_t message(img_str.size());
+    // copy the data to the message
+    memcpy(message.data(), img_str.data(), img_str.size());
+    ps.send(message);
 
-  return 0;
+    return 0;
 }
